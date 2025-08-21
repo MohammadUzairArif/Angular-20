f9124261bd36134177ba48e67beda12d1ae66299
user.name like : "uzairarif28@gmail.com <uzairarif28@gmail.com>
2025-08-21 21:32:57 +0500

    variable and data binding

---
+++ src/app/components/data-binding/data-binding.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-data-binding',
+  imports: [FormsModule],
+  templateUrl: './data-binding.html',
+  styleUrl: './data-binding.css'
+})
+export class DataBinding {
+  // string, num, boolean, date, array, object, any
+  courseName: string = 'Angular 20';
+  courseDuration: number = 30; // in days
+  isCourseActive: boolean = false;
+  courseStartDate: Date = new Date(); 
+
+  minTestScore: number = 50;
+ spanClass: string = 'secondary';
+  constructor() {
+    console.log(this.courseName);
+    this.courseName = 'Angular 20 Data Binding';
+    console.log(this.courseName); 
+
+    setTimeout(() => {
+      this.courseDuration = 45; // Update course duration after 7 seconds
+    }, 7000);
+  }
+
+  
+}
