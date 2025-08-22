7cfc26dd0c358bf1b772f92f7de91fd5b652aa6f
user.name like : "uzairarif28@gmail.com <uzairarif28@gmail.com>
2025-08-23 00:12:29 +0500

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { ControlFlow } from './components/control-flow/control-flow';
+import { DataBinding } from './components/data-binding/data-binding';
+import { Signal } from './components/signal/signal';
+import { Attribute } from './components/attribute/attribute';
+import { Notfound } from './components/notfound/notfound';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        redirectTo: "data-binding",
+        pathMatch: "full"
+    },
+    {
+        path:"controlflow",
+        component: ControlFlow
+    },
+    {
+        path: "data-binding",
+        component: DataBinding
+    },
+    {
+        path: "signal",
+        component: Signal
+    },
+    {
+        path: "attribute",
+        component: Attribute
+    },
+    {
+        path: "**",
+        component: Notfound
+    }
+];
